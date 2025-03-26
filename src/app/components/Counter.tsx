94b9855a385199049c31f4145aa456678e26095f
baolinhdev <baolinhdev@gmail.com>
2025-03-26 10:51:18 +0700

    useState Counter App

--- src/app/components/Counter.tsx
+++ src/app/components/Counter.tsx
@@ -1,11 +1,17 @@
+"use client"
+import { useState } from "react";
+
 type CounterProps = {
   initialCount: number;
 };
 
-const Counter: React.FC<CounterProps> = ({ initialCount }) => {
+const Counter: React.FC<CounterProps> = ({ initialCount }) => { 
+  const [count, setCount] = useState(initialCount)
   return (
     <div className="border p-4 rounded-lg shadow-md">
-      <h2>Giá trị ban đầu: {initialCount}</h2>
+      <h2>Giá trị ban đầu: {initialCount}</h2> 
+      <button onClick={() => setCount(count + 1)}>Tăng</button> 
+      <h2>Giá trị giá trị hiện tại: {count}</h2> 
     </div>
   );
 };
