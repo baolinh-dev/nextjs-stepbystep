694493870712b89587c265c3bc2a426ae6db947b
baolinhdev <baolinhdev@gmail.com>
2025-03-26 10:33:18 +0700

    extend data with &

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,11 +1,16 @@
-type CardProps = {
+type BaseProps = {
     title: string;
     description: string;
   };
   
-  const Card: React.FC<CardProps> = ({ title, description }) => {
+  type AdvancedCardProps = BaseProps & {
+    imageUrl?: string;
+  };
+  
+  const Card: React.FC<AdvancedCardProps> = ({ title, description, imageUrl }) => {
     return (
       <div className="border p-4 rounded-lg shadow-md">
+        {imageUrl && <img src={imageUrl} alt={title} className="w-full h-32 object-cover rounded-lg mb-4" />}
         <h2 className="text-xl font-bold">{title}</h2>
         <p className="text-gray-600">{description}</p>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div>
       <h1 className="text-2xl font-bold mb-4">Danh sách thẻ</h1>
       <Card title="Thẻ 1" description="Đây là thẻ đầu tiên." />
-      <Card title="Thẻ 2" description="Đây là thẻ thứ hai." />
+      <Card title="Thẻ 2" description="Đây là thẻ thứ hai."  imageUrl="https://images.unsplash.com/photo-1508138221679-760a23a2285b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
     </div>
   );
 }
