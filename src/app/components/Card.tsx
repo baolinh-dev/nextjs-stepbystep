ba4fbad1d7f05699cc17fd7cc2d83adee2f4e10e
baolinhdev <baolinhdev@gmail.com>
2025-03-26 10:28:19 +0700

    type data

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,15 +1,16 @@
-interface CardProps {
-  title: string;
-  description: string;
-}
-
-const Card: React.FC<CardProps> = ({ title, description }) => {
-  return (
-    <div className="border p-4 rounded-lg shadow-md">
-      <h2 className="text-xl font-bold">{title}</h2>
-      <p className="text-gray-600">{description}</p>
-    </div>
-  );
-};
-
-export default Card;
+type CardProps = {
+    title: string;
+    description: string;
+  };
+  
+  const Card: React.FC<CardProps> = ({ title, description }) => {
+    return (
+      <div className="border p-4 rounded-lg shadow-md">
+        <h2 className="text-xl font-bold">{title}</h2>
+        <p className="text-gray-600">{description}</p>
+      </div>
+    );
+  };
+  
+  export default Card;
+  
